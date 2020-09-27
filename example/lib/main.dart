01257dc59577d18bed3fbee5d1d790eeb85e68f2
xuefeng.lly <xuefeng.lly@autonavi.com>
2020-09-27 16:16:43 +0800

    feat: 添加flutter层，开启定位的接口

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
-
 import 'package:amap_maps_flutter/amap_maps_flutter.dart';
+import 'package:permission_handler/permission_handler.dart';
+import 'dart:async';
 
 void main() => runApp(MyApp());
 
@@ -27,9 +28,13 @@ class MyHomePage extends StatefulWidget {
   _MyHomePageState createState() => _MyHomePageState();
 }
 
-
 class _MyHomePageState extends State<MyHomePage> {
-
+  @override
+  void initState() {
+    super.initState();
+    // 动态申请定位权限
+    requestPermission();
+  }
 
   AMapController mapController;
 
@@ -52,64 +57,79 @@ class _MyHomePageState extends State<MyHomePage> {
                   child: AMap(
                     onMapCreated: onMapCreated,
                   ))),
-          Row(children: <Widget>[
-            FlatButton(
-              child: Text("移动到北京"),
-              onPressed: () {
-                if (mapController != null) {
-                  mapController.changeCamera(CameraPosition(
-                      target: LatLng(39.893927, 116.405972),
-                      zoom: 10
-                  ), _isAnimation);
-                }
-              },),
-            _isAnimationButton(),
-          ],),
-          Row(children: <Widget>[
-            FlatButton(
-              child: Text("添加Marker"),
-              onPressed: () {
-                if (mapController != null) {
-                  MarkerOptions options = MarkerOptions.defaultOptions;
-
-                  mapController.addMarker(options).then(
-                          (marker) {
-                        setState(() {
-                          currentMarker = marker;
-                        }
-                        );
-                      }
-                  );
-                }
-              },),
-            FlatButton(
-              child: Text("修改位置"),
-              onPressed: () {
-                if (mapController != null) {
-                  if (currentMarker != null) {
-                    //fixme 如果默认值不一样如何更新
-                    MarkerOptions markerOptions = MarkerOptions(
-                        position: LatLng(
-                            currentMarker.options.position.latitude,
-                            currentMarker.options.position.longitude + 0.001));
-                    mapController.updateMarker(currentMarker, markerOptions);
-                  } else {
-                    print("currentMarker is null");
+          Row(
+            children: <Widget>[
+              FlatButton(
+                child: Text("移动到北京"),
+                onPressed: () {
+                  if (mapController != null) {
+                    mapController.changeCamera(
+                        CameraPosition(
+                            target: LatLng(39.893927, 116.405972), zoom: 10),
+                        _isAnimation);
                   }
-                }
-              },),
-          ],),
-          Row(children: <Widget>[
-            FlatButton(
-              child: Text("添加Polyline"),
-              onPressed: () {
-                if (mapController != null) {
-                  MarkerOptions options = MarkerOptions.defaultOptions;
-                  mapController.addMarker(options);
-                }
-              },),
-          ],),
+                },
+              ),
+              _isAnimationButton(),
+            ],
+          ),
+          Row(
+            children: <Widget>[
+              FlatButton(
+                child: Text("添加Marker"),
+                onPressed: () {
+                  if (mapController != null) {
+                    MarkerOptions options = MarkerOptions.defaultOptions;
 
+                    mapController.addMarker(options).then((marker) {
+                      setState(() {
+                        currentMarker = marker;
+                      });
+                    });
+                  }
+                },
+              ),
+              FlatButton(
+                child: Text("修改位置"),
+                onPressed: () {
+                  if (mapController != null) {
+                    if (currentMarker != null) {
+                      //fixme 如果默认值不一样如何更新
+                      MarkerOptions markerOptions = MarkerOptions(
+                          position: LatLng(
+                              currentMarker.options.position.latitude,
+                              currentMarker.options.position.longitude +
+                                  0.001));
+                      mapController.updateMarker(currentMarker, markerOptions);
+                    } else {
+                      print("currentMarker is null");
+                    }
+                  }
+                },
+              ),
+              FlatButton(
+                child: Text("显示位置"),
+                onPressed: () {
+                  if (mapController != null) {
+                    mapController.setShowUserLocation(true);
+                  }
+                },
+              ),
+            ],
+          ),
+          Row(
+            children: <Widget>[
+              FlatButton(
+                child: Text("添加Polyline"),
+                onPressed: () {
+                  if (mapController != null) {
+                    MarkerOptions options = MarkerOptions.defaultOptions;
+                    mapController.addMarker(options);
+                  }
+                },
+              ),
+            ],
+          ),
         ]));
   }
 
@@ -124,7 +144,6 @@ class _MyHomePageState extends State<MyHomePage> {
     );
   }
 
-
   void onMapCreated(AMapController controller) {
     print("onMapCreated");
     mapController = controller;
@@ -134,7 +153,6 @@ class _MyHomePageState extends State<MyHomePage> {
     mapController.onCameraChanged.add(onCameraChanged);
   }
 
-
   void onMapLoaded(argument) {
     print("onMapLoaded");
   }
@@ -143,5 +161,33 @@ class _MyHomePageState extends State<MyHomePage> {
     print("onCameraChanged " + onCameraChanged.toString());
   }
 
+  /// 动态申请定位权限
+  void requestPermission() async {
+    // 申请权限
+    bool hasLocationPermission = await requestLocationPermission();
+    if (hasLocationPermission) {
+      print("定位权限申请通过");
+    } else {
+      print("定位权限申请不通过");
+    }
+  }
 
+  /// 申请定位权限
+  /// 授予定位权限返回true， 否则返回false
+  Future<bool> requestLocationPermission() async {
+    //获取当前的权限
+    var status = await Permission.location.status;
+    if (status == PermissionStatus.granted) {
+      //已经授权
+      return true;
+    } else {
+      //未授权则发起一次申请
+      status = await Permission.location.request();
+      if (status == PermissionStatus.granted) {
+        return true;
+      } else {
+        return false;
+      }
+    }
+  }
 }
--- lib/src/AMapController.dart
+++ lib/src/AMapController.dart
@@ -1,22 +1,24 @@
 part of amap_maps_flutter;
 
-class AMapController extends ChangeNotifier{
+class AMapController extends ChangeNotifier {
+  static const String CHANNEL =
+      "plugins.flutter.maps.amap.com/amap_maps_flutter";
 
-  static const String CHANNEL = "plugins.flutter.maps.amap.com/amap_maps_flutter";
-
-  static const String METHOD_NAME_CALLBACK_AMAP_ON_MAP_LOADED = "amap#onMapLoaded";
-  static const String METHOD_NAME_CALLBACK_AMAP_ON_CAMERA_CHANGE = "amap#onCameraChange";
+  static const String METHOD_NAME_CALLBACK_AMAP_ON_MAP_LOADED =
+      "amap#onMapLoaded";
+  static const String METHOD_NAME_CALLBACK_AMAP_ON_CAMERA_CHANGE =
+      "amap#onCameraChange";
   static const String MEHTOD_NAME_AMAP_CHANGE_CAMERA = "amap#changeCamera";
   static const String MEHTOD_NAME_AMAP_ADD_MARKER = "amap#addMarker";
   static const String MEHTOD_NAME_AMAP_UPDATE_MARKER = "amap#updateMarker";
+  static const String METHOD_NAME_AMAP_SHOW_USER_LOCATION =
+      "amap#showUserLocation";
 
   final int _id;
 
   AMapController._(this._id)
-      : assert(_id != null)
-  ,
-        _channel = new MethodChannel(CHANNEL + _id.toString())
-  {
+      : assert(_id != null),
+        _channel = new MethodChannel(CHANNEL + _id.toString()) {
     _channel.setMethodCallHandler(_handleMethodCall);
   }
 
@@ -27,10 +29,9 @@ class AMapController extends ChangeNotifier{
     return AMapController._(id);
   }
 
-
   /// 地图状态发生变化的监听接口。
-  final ArgumentCallbacks<CameraPosition> onCameraChanged = ArgumentCallbacks<
-      CameraPosition>();
+  final ArgumentCallbacks<CameraPosition> onCameraChanged =
+      ArgumentCallbacks<CameraPosition>();
 
   /// 地图状态发生变化的监听接口。
   final ArgumentCallbacks onMapLoaded = ArgumentCallbacks();
@@ -39,22 +40,20 @@ class AMapController extends ChangeNotifier{
   Set<Marker> get markers => Set<Marker>.from(_markers.values);
   final Map<String, Marker> _markers = <String, Marker>{};
 
-
   Future<dynamic> _handleMethodCall(MethodCall call) async {
     switch (call.method) {
       case METHOD_NAME_CALLBACK_AMAP_ON_MAP_LOADED:
         onMapLoaded.call(null);
         break;
       case METHOD_NAME_CALLBACK_AMAP_ON_CAMERA_CHANGE:
-        CameraPosition cameraPosition = CameraPosition.fromMap(
-            call.arguments['position']);
+        CameraPosition cameraPosition =
+            CameraPosition.fromMap(call.arguments['position']);
         onCameraChanged.call(cameraPosition);
         break;
       default:
         throw MissingPluginException();
     }
   }
-
 
   /// 地图操作
   void changeCamera(CameraPosition cameraPosition, bool isAnimate) {
@@ -64,12 +63,10 @@ class AMapController extends ChangeNotifier{
     }
   }
 
-
   /// 覆盖物添加
   Future<Marker> addMarker(MarkerOptions options) async {
-
     final MarkerOptions effectiveOptions =
-    MarkerOptions.defaultOptions.copyWith(options);
+        MarkerOptions.defaultOptions.copyWith(options);
     final String markerId = await _channel.invokeMethod(
       MEHTOD_NAME_AMAP_ADD_MARKER,
       <String, dynamic>{
@@ -90,7 +87,8 @@ class AMapController extends ChangeNotifier{
     assert(marker != null);
     assert(_markers[marker._id] == marker);
     assert(changes != null);
-    await _channel.invokeMethod(MEHTOD_NAME_AMAP_UPDATE_MARKER, <String, dynamic>{
+    await _channel
+        .invokeMethod(MEHTOD_NAME_AMAP_UPDATE_MARKER, <String, dynamic>{
       'marker': marker._id,
       'options': changes._toJson(),
     });
@@ -99,8 +97,15 @@ class AMapController extends ChangeNotifier{
     notifyListeners();
   }
 
+  /// 是否显示位置
+  void setShowUserLocation(bool showUserLocation) {
+    if (_channel != null) {
+      _channel.invokeMethod(
+          METHOD_NAME_AMAP_SHOW_USER_LOCATION, showUserLocation);
+    }
+  }
 
-///
-/// 工具转换
+  ///
+  /// 工具转换
 
 }
